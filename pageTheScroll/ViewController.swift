d3391f4d56849fbb3ce9e8e763ec49a358b24694
Omar Wilson <omar.wilson3@comcast.net>
2016-11-17 10:20:38 -0800

    Loaded images and icons, created images array

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...2 {
+            let image = UIImage(named: "icon\(i).png")
+            images.append(UIImageView(image: image))
+        }
+        
+        print("Count: \(images.count)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
