b0c51cbf38c98afc160049c7e71c1cdbfec9195e
Omar Wilson <omar.wilson3@comcast.net>
2016-11-17 16:13:26 -0800

    Three icons in scroll view

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -10,20 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var images = [UIImageView]()
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in 0...2 {
-            let image = UIImage(named: "icon\(i).png")
-            images.append(UIImageView(image: image))
+        
         }
         
-        print("Count: \(images.count)")
+        // scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        for i in 0...2 {
+            let scrollWidth = scrollView.frame.size.width
+            let image = UIImage(named: "icon\(i).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(i)
+            contentWidth += newX
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+    }
+    scrollView.clipsToBounds = false
+    scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
-
-
 }
+
+
 
